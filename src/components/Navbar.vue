8ade53fb3e79853a81710ec8274351e703ff5879
Adit Tanu <adiettanu27@gmail.com>
2023-01-09 22:38:27 +0700

    chatroom

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <div>
+      <p>Hi there ... display name</p>
+      <p class="email">currently login as ... email</p>
+    </div>
+    <button>Logout</button>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
